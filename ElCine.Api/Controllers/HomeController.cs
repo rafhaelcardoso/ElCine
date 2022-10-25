9fc26d12e345e7188718f7fddb51912e3f03f328
Cardoso <Cardoso@RAFHAEL-PC>
2022-10-25 01:13:43 -0300

    Create Video Model and HomeController

---
+++ ElCine.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElCine.Api.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ElCine.Api/Models/Video.cs
@@ -0,0 +1,10 @@
+﻿namespace ElCine.Api.Models
+{
+    public class Video
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+    }
+}
